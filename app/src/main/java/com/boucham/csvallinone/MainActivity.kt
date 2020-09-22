03bd974336cb22fcc01dbeeb998baf1bb8790a23
NOPROD <contact@boucham-amine.fr>
2020-09-22 04:40:44 +0200

    :sparkles: : Clean PagerAdapter

--- app/src/main/java/com/boucham/csvallinone/MainActivity.kt
+++ app/src/main/java/com/boucham/csvallinone/MainActivity.kt
@@ -2,34 +2,24 @@ package com.boucham.csvallinone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.boucham.csvallinone.core.extends.addFragment
-import com.boucham.csvallinone.core.extends.replaceFragment
-import com.boucham.csvallinone.main.ui.FragmentMain
-import com.boucham.csvallinone.main.ui.FragmentSettings
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.viewpager.widget.ViewPager
+import com.boucham.csvallinone.core.extends.SectionsPagerAdapter
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
 
-        val fragmentMain = FragmentMain()
-        val fragmentSettings = FragmentSettings()
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        val tabs: TabLayout = findViewById(R.id.tabs)
 
-        replaceFragment(fragmentMain, R.id.flFragment)
-    }
+        viewPager.adapter = sectionsPagerAdapter
+        tabs.setupWithViewPager(viewPager)
 
-
-    fun initActions() {
-        btnFragmentSettings.setOnClickListener { }
-        btnFragmentFavorites.setOnClickListener { }
-        btnFragmentFiles.setOnClickListener { }
-        btnFragmentRead.setOnClickListener { }
-        btnFragmentSearch.setOnClickListener { }
-    }
-
-    fun replace() {
 
     }
+
 }
---
+++ app/src/main/java/com/boucham/csvallinone/core/extends/SectionsPagerAdapter.kt
@@ -0,0 +1,46 @@
+package com.boucham.csvallinone.core.extends
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.boucham.csvallinone.R
+import com.boucham.csvallinone.main.ui.FragmentFiles
+import com.boucham.csvallinone.main.ui.FragmentFolders
+import com.boucham.csvallinone.main.ui.FragmentSearch
+import com.boucham.csvallinone.main.ui.FragmentSettings
+
+private val TAB_TITLES = mapOf<Int, Int>(
+    0 to R.string.title_files,
+    1 to R.string.title_folder,
+    2 to R.string.title_search,
+    3 to R.string.title_settings
+)
+
+private val TAB_FRAGMENTS = mapOf<Int, Any>(
+    0 to FragmentFiles(),
+    1 to FragmentFolders(),
+    2 to FragmentSearch(),
+    3 to FragmentSettings()
+)
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    override fun getItem(position: Int): Fragment {
+        return TAB_FRAGMENTS.getValue(position) as Fragment
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES.getValue(position))
+    }
+
+    override fun getCount(): Int {
+        // Show 2 total pages.
+        return 4
+    }
+}
