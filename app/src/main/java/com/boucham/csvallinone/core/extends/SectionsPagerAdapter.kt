652455df9b85f78d7cc083be70ea566fc31993cf
NOPROD <contact@boucham-amine.fr>
2020-09-22 04:44:17 +0200

    :art: : Generic getCount

--- app/src/main/java/com/boucham/csvallinone/core/extends/SectionsPagerAdapter.kt
+++ app/src/main/java/com/boucham/csvallinone/core/extends/SectionsPagerAdapter.kt
@@ -40,7 +40,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 4
+        return TAB_FRAGMENTS.size
     }
 }
